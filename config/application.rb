923eec3c4bfc9d66f958bc6362a6a03cdca2e037
Sandro Duarte <sandrods@gmail.com>
2014-03-31 23:10:02 -0300

    rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 Date::DATE_FORMATS[:default] = lambda { |date| I18n.l(date) }
 
