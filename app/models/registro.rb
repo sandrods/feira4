6ed486c531c1dadc4a4f67f0d47c4cd8b97677b0
Sandro Duarte <sandrods@gmail.com>
2014-03-31 23:08:04 -0300

    categorias de registros

--- app/models/registro.rb
+++ app/models/registro.rb
@@ -1,10 +1,10 @@
 class Registro < ActiveRecord::Base
-  
+
   # acts_as_br_date :data
   # acts_as_br_currency :valor
-  
+
   validates_presence_of :data, :descricao, :valor, :cd
-  
+
   scope :creditos, where(cd: 'C')
   scope :debitos,  where(cd: 'D')
 
@@ -12,6 +12,7 @@ class Registro < ActiveRecord::Base
 
   belongs_to :conta
   belongs_to :registravel, polymorphic: true
+  belongs_to :categoria
 
   def valor_cd
     valor * (cd=="D" ? -1 : 1)
