3ddd743f549e774ea392b39c78f6746caa5ab352
Sandro Duarte <sandrods@gmail.com>
2016-03-22 00:10:02 -0300

    clean up

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,20 +1,14 @@
 class Venda < ActiveRecord::Base
   include ClienteOuVendedor
 
-  has_many :itens, class_name: "ItemVenda", dependent: :destroy do
-    def from_barcode(bc)
-      #ItemVenda.from_barcode(bc, proxy_association.owner.id)
-      item = Item.from_barcode(bc)
-      proxy_association.owner.adiciona_item!(item)
-    end
-  end
+  has_many :itens, class_name: "ItemVenda", dependent: :destroy
 
   has_many :pagamentos, -> { where(cd: "C").order('data asc') },  class_name: "Registro", as: :registravel
 
   def self.from_sacola(sacola_id)
     sacola = Sacola.find(sacola_id)
 
-    venda = Venda.create!(data: Date.today, cliente_id: sacola.cliente_id, tipo: 'C', desconto: 0)
+    venda = Venda.create!(data: Date.today, cliente_id: sacola.cliente_id, tipo: sacola.tipo, desconto: 0)
 
     sacola.itens.incluidos.each { |item_sacola| ItemVenda.create_by_item(item_sacola.item, self) }
 
