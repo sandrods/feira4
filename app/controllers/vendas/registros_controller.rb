a348e3a2220f4ad35f45e89696d838ad464bf8ba
Sandro Duarte <sandrods@gmail.com>
2016-03-23 10:48:08 -0300

    exclusão de pagamentos na venda

--- app/controllers/vendas/registros_controller.rb
+++ app/controllers/vendas/registros_controller.rb
@@ -5,6 +5,10 @@ class Vendas::RegistrosController < ApplicationController
     @registro = @venda.pagamentos.create registro_params.merge(descricao: "Venda #{@venda.id}: #{@venda.nome}")
   end
 
+  def destroy
+    @venda.pagamentos.find(params[:id]).destroy!
+  end
+
  private
 
   def registro_params
