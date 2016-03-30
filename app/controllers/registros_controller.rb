07d743740a169c508fb4267a5a29a9d636f3384a
Sandro Duarte <sandrods@gmail.com>
2016-03-30 13:07:12 -0300

    edição e inclusão de registros

--- app/controllers/registros_controller.rb
+++ app/controllers/registros_controller.rb
@@ -4,12 +4,35 @@ class RegistrosController < ApplicationController
   end
 
   def new
-    @registro = Registro.new
+    @registro = Registro.new(cd: params[:cd])
+    render layout: false
+  end
+
+  def create
+    @registro = Registro.new registro_params
+    @registro.save!
+
+    redirect_to registros_path
   end
 
   def edit
     @registro = Registro.find params[:id]
     render layout: false
   end
+
+  def update
+    @registro = Registro.find params[:id]
+    @registro.update! registro_params
+
+    redirect_to registros_path
+  end
+
+  private
+
+   def registro_params
+     params.require(:registro)
+           .permit(:data, :valor, :conta_id, :forma_id, :descricao, :pago, :cd)
+           .delocalize(data: :date, valor: :number)
+   end
 
 end
--- app/helpers/registros_helper.rb
+++ app/helpers/registros_helper.rb
@@ -1,2 +1,11 @@
 module RegistrosHelper
+
+  def label_cd(registro)
+    if registro.receita?
+      content_tag(:span, 'RECEITA', class: "label label-success")
+    else
+      content_tag(:span, 'DESPESA', class: "label label-danger")
+    end
+  end
+
 end
