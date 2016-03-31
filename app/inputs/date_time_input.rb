ab3f0cf25468cdb57e4aac28ded8bcc84ed7736c
Sandro Duarte <sandrods@gmail.com>
2016-03-30 22:12:43 -0300

    so um dateTimeInput

--- app/inputs/date_time_input.rb
+++ app/inputs/date_time_input.rb
@@ -1,11 +1,11 @@
 class DateTimeInput < SimpleForm::Inputs::DateTimeInput
 
-  def input
+  def input(wrapper_options)
     @builder.text_field(attribute_name, input_html_options.merge(:autocomplete => 'off'))
   end
 
   def input_html_classes
-    super.push('datepicker input-small')
+    super.push('datepicker input-sm form-control')
   end
 
 end
--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -134,15 +134,3 @@ SimpleForm.setup do |config|
     boolean: :vertical_boolean,
   }
 end
-
-class DateTimeInput < SimpleForm::Inputs::DateTimeInput
-
-  def input(wrapper_options)
-    @builder.text_field(attribute_name, input_html_options.merge(:autocomplete => 'off'))
-  end
-
-  def input_html_classes
-    super.push('datepicker input-sm form-control')
-  end
-
-end
