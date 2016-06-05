6d273005b6938d25892ab2362cd7ca4785252079
Sandro Duarte <sandrods@gmail.com>
2016-06-05 20:59:22 -0300

    recalcular combo de colecão sempre

--- app/models/colecao.rb
+++ app/models/colecao.rb
@@ -3,11 +3,7 @@ class Colecao < ActiveRecord::Base
   default_scope -> { order('id desc') }
 
   def Colecao.to_select
-    @@combo ||= Colecao.update_select
-  end
-
-  def Colecao.update_select
-    @@combo = Colecao.all.map {|c| [c.descricao, c.id]}
+    Colecao.all.map {|c| [c.descricao, c.id]}
   end
 
 end
