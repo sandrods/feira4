c5350424014cbb60ab18a944d9b480f25a9d0765
Sandro Duarte <sandrods@gmail.com>
2016-06-15 00:03:00 -0300

    inicio do trabalho em transferencias

---
+++ app/models/transferencia.rb
@@ -0,0 +1,32 @@
+class Transferencia
+  include ActiveModel::Model
+  attr_accessor :conta_origem, :conta_destino, :data, :valor
+
+  validates :conta_origem, :conta_destino, :data, :valor, presence: true
+
+  def create
+    return false unless valid?
+
+    origem = Conta.find :conta_origem
+    destino = Conta.find :conta_destino
+
+    Conta.transaction do
+
+      o = origem.registros.create! data: data,
+                                    descricao: "Transferência #{origem.nome} -> #{destino.nome}",
+                                    valor: valor,
+                                    cd: "D"
+
+      d = origem.registros.create! data: data,
+                                    descricao: "Transferência #{origem.nome} -> #{destino.nome}",
+                                    valor: valor,
+                                    cd: "C",
+                                    transf_id: o.id
+
+      o.update_columns transf_id: d.id
+
+    end
+
+  end
+
+end
