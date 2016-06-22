d8e6d34296ce0c6d4761abce9f4abb2274fb9c02
Sandro Duarte <sandrods@gmail.com>
2016-06-21 23:11:50 -0300

    criar link simbolico para o ultimo backup

--- db/backup.sh
+++ db/backup.sh
@@ -15,7 +15,8 @@ DST="/Users/sandro/Dropbox/pg_bkp"
 if [ -d /Users/sandro/Dropbox/pg_bkp ]; then
   cd $DST
 else
-`mkdir $DST`
+  `mkdir $DST`
+  cd $DST
 fi
 
 # Define permissoes de leitura e gravacao para o diretorio
@@ -23,14 +24,10 @@ fi
 # `chown -R postgres /opt/data/backup/$t`
 # `chmod 0777 /opt/data/backup/$t`
 
-file="$DST/$t"-"$tt"-"$db.bkp"
+file="$t"-"$tt"-"$db.bkp"
 
 pg_dump $db -Fc -f $file
 
-echo "BackUp to $file"
-# Loop para gerar arquivos dump
-# for i in `psql -l -U postgres | cut -f 2 -d " " -s`; do
-#     if [ $i != template1 -a $i != template0 -a $i != "rows)" -a $i != postgres ]; then
-#         su postgres -c "/usr/bin/pg_dump -h localhost -U postgres -c -d -F -f $DST/$t/$i"_"$t"_"$tt.bkp -Z 1 $i";
-#     fi
-# done
+echo "BackUp to $DST/$file"
+
+ln -f -s $file last.bkp
