56dbe7638468e4a77105e834dc5ec9a21154a8d1
Sandro Duarte <sandrods@gmail.com>
2016-09-26 23:01:16 -0300

    faltou

--- db/migrate/20160915232555_add_counter_cache_to_clientes.rb
+++
@@ -1,6 +0,0 @@
-class AddCounterCacheToClientes < ActiveRecord::Migration
-  def change
-    add_column :clientes, :vendas_count, :integer
-    Cliente.all.each { |c| Cliente.reset_counters c.id, :vendas }
-  end
-end
