544868dae2f85d68724730b1d647426d338c19bd
Sandro Duarte <sandrods@gmail.com>
2018-03-21 11:46:16 -0300

    gráficos de quantidades de venda

--- app/models/venda/grafico.rb
+++ app/models/venda/grafico.rb
@@ -2,18 +2,36 @@ class Venda::Grafico
 
   attr_accessor :calendar
 
+  delegate :year, to: :calendar
+
   def initialize(data)
     @calendar = Calendar.new(data)
     @vendas = Venda.where(data: @calendar.range)
     @itens = ItemEstoque.vendidos.where(movimento_id: @vendas.pluck(:id))
+
+    @itens_por_mes = @itens.to_a.group_by_month { |i| i.movimento.data }
   end
 
   def quantidade_por_mes
-    Hash[@itens.to_a.group_by_month { |i| i.movimento.data }.map { |k, v| [k.to_s(:db), v.count] }]
+    all_months Hash[@itens_por_mes.map { |k, v| [k, v.count] }]
   end
 
   def valor_por_mes
-    Hash[@itens.to_a.group_by_month(format: "%b %Y") { |i| i.movimento.data }.map { |k, v| [k, v.sum(&:valor)] }]
+    all_months Hash[@itens_por_mes.map { |k, v| [k, v.sum(&:valor)] }]
+  end
+
+  def last_year
+    @last ||= Venda::Grafico.new @calendar.last_year
+  end
+
+  def all_months(serie)
+    ret = {}
+    (1..12).each do |mes|
+      dt = Date.new(@calendar.year, mes, 1)
+      dt_fmt = dt.strftime('%b %Y')
+      ret[dt_fmt] = serie.fetch(dt, 0)
+    end
+    ret
   end
 
   # def valor_por_mes
